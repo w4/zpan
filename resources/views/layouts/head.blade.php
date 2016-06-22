270d517d138471dcabe375811dbbbb863e383c27
Jordan Doyle <jordan@doyle.wf>
2016-06-22 03:54:48 +0100

    Reminder that I am everywhere

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="author" content="Jordan Doyle">
+<link rel="author" href="humans.txt">
 
 <title>@yield('title')- zpan</title>
 
