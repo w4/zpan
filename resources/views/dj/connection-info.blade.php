36c8f6e5b8326104c29486ff95a6bf6fd5358256
Jordan Doyle <jordan@doyle.wf>
2016-06-22 19:00:52 +0100

    SHOUTcast v2 info

--- resources/views/dj/connection-info.blade.php
+++ resources/views/dj/connection-info.blade.php
@@ -12,13 +12,13 @@
 
             <p>{{ _('Unlike many fansites, we use up-to-date software. This also means we are using SHOUTcast 2. Don\'t worry, you can still use SAM Broadcaster 2 to connect to the server. Please ensure you paste the password exactly how it is displayed below. If your broadcast software supports it, please select SHOUTcast 2, otherwise you can click SHOUTcast 1.') }}</p>
 
-            <p><strong>{{ _('IMPORTANT:') }}</strong> {{ _('Anybody without their panel username in their broadcast password will be kicked from the radio and will be given a warning. Broadcasting at the wrong bitrate will also result in a warning.') }}</p>
+            <p><strong>{{ _('IMPORTANT:') }}</strong> {{ _('Anybody without their panel username in their broadcast password will be kicked from the radio and will be given a warning. Broadcasting at the wrong bitrate will also result in a warning.') }} <strong>{{ _('ALWAYS PREFER SHOUTCAST 2 OVER SHOUTCAST 1.') }}</strong></p>
         </div>
     </div>
 
     <div class="mdl-card mdl-cell mdl-cell--12-col mdl-shadow--2dp">
         <div class="mdl-card__supporting-text">
-            <h4>{{ _('Connection Information') }}</h4>
+            <h4>{{ _('SHOUTcast v1 Connection Information') }}</h4>
             @if($connection)
                 <p>
                     {!! __('IP: %s', "<strong>{$connection->ip}</strong>") !!}<br>
@@ -31,4 +31,23 @@
             @endif
         </div>
     </div>
+
+    @if($connection)
+        <div class="mdl-card mdl-cell mdl-cell--12-col mdl-shadow--2dp">
+            <div class="mdl-card__supporting-text">
+                <h4>{{ _('SHOUTcast v2 Connection Information') }}</h4>
+                <p>
+                    {!! __('IP: %s', "<strong>{$connection->ip}</strong>") !!}<br>
+                    {!! __('Port: %s', "<strong>{$connection->port}</strong>") !!}<br>
+                    {!! __('Stream ID: %s', '<strong>1</strong>') !!}<br>
+                    {!! __('DJ/User ID: %s', '<strong>' . auth()->user()->username . '</strong>') !!}
+                    {!! __('Password: %s', "<strong>{$connection->password}</strong>") !!}
+                    {!! __('Use SHOUTcast v1 mode: %s', '<strong>UNCHECKED</strong>') !!}
+                    <br>
+                    {!! __('Bitrate: %s', '<strong>256kbps</strong>') !!}
+                </p>
+            </div>
+        </div>
+    @endif
+
 @endsection
